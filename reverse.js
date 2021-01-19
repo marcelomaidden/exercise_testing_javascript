0b750199912fd935f91f62d9fff1fd10c6b3c770
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-19 12:48:21 -0300

    Add more validations on reverse string

--- reverse.js
+++ reverse.js
@@ -1,11 +1,18 @@
 const reverseString = (string) => {
-  let reversed = "";
-  let count = string.length - 1
-  while (count >= 0) {
-    reversed += string[count]
-    count -= 1
+  if (typeof(string) !== 'string')
+    return 'Enter a valid string'
+  else if (string.trim() === '')
+    return 'You entered an empty string'
+  else
+  {
+    let reversed = "";
+    let count = string.length - 1
+    while (count >= 0) {
+      reversed += string[count]
+      count -= 1
+    }
+    return reversed
   }
-  return reversed
 }
 
 module.exports = reverseString
--- reverse.test.js
+++ reverse.test.js
@@ -4,3 +4,11 @@ const reverseString = require('./reverse');
 test("Reverse a string", () => {
   expect(reverseString("marcelo")).toBe("olecram");
 })
+
+test("Test if user entered an empty string", () => {
+  expect(reverseString("")).toBe("You entered an empty string");
+})
+
+test("Test if input is valid", () => {
+  expect(reverseString(1)).toBe("Enter a valid string");
+})
