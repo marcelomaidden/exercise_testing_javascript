13b8e81a44d06faa88eb401922af1cb022f5651c
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-01-19 12:44:56 -0300

    Add valid reverseString method

--- reverse.js
+++ reverse.js
@@ -1,5 +1,11 @@
 const reverseString = (string) => {
-  return null
+  let reversed = "";
+  let count = string.length - 1
+  while (count >= 0) {
+    reversed += string[count]
+    count -= 1
+  }
+  return reversed
 }
 
 module.exports = reverseString
